aa5b6ba238a1b60ced3ebbc5d84210ad7ddd7824
Silvestre-P <esilvert12@gmail.com>
2026-05-12 10:23:13 -0700

    Update project title and description for clarity

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -33,9 +33,9 @@ const CONFIG = {
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Project Name',
+          title: 'Evolutionary Processes and diversity patterns of European dragonfiles',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            'Investigating the historical factors to the European dragonfly diversity',
           imageUrl:
             'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
           link: 'https://example.com',
